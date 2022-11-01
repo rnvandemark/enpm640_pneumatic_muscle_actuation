1334a4da8170bc936c7b9258338589be55f03aa1
R. Nick Vandemark <rnvandemark@gmail.com>
2022-10-31 20:17:28 -0400

    Use MoveIt! setup assistant to create initial, basic setup for planar 2DOF arm control
    This was generated using the URDF file at pma_description/urdf/pma_planar_2dof.urdf.

---
+++ pma_description/launch/moveit_rviz.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_moveit_rviz_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("pma_planar_2dof", package_name="pma_description").to_moveit_configs()
+    return generate_moveit_rviz_launch(moveit_config)
