e754aebdb541609e7846d8b7e6a862f126d40e9b
R. Nick Vandemark <rnvandemark@gmail.com>
2022-12-08 17:42:03 -0500

    Add a simple collection of core pressure-space trajectory types.

---
+++ pma_util/include/pma_util/pma_trajectory_types.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+
+namespace pma_util
+{
+/**
+ * Define the precision for a pressure value.
+ */
+using Pressure = double;
+/**
+ * Define a type for a list of pressures.
+ */
+using PressureList = std::vector<Pressure>;
+}
